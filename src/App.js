827a0c4447692e4607ba68d439aa04a1244c29c2
Harovinkin <kharovinkin@gmail.com>
2020-11-12 13:13:09 +0200

    useState_counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, {useState, Fragment} from 'react'
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(prev => prev + 1)
+  }
 
+  function dicrement() {
+    setCounter(prev => prev - 1)
+  }
+
+  return(
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increment}>+1</button>
+      <button onClick={dicrement}>-1</button>
+    </>
+  )
 }
 
 export default App;
