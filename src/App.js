c5882b104555f74b7d3d0325cc630818f693db37
Harovinkin <kharovinkin@gmail.com>
2020-11-18 17:57:00 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
 
+  const [value, setValue] = useState('');
+
+  const currentRender = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef('');
+
+  useEffect(() => {
+    currentRender.current++
+  })
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value])
+
+  const focus = () => {
+    inputRef.current.focus();
+  }
+
+return (
+  <>
+    <h1>Renders Quantity: {currentRender.current}</h1>
+    <h1>Current Value: {prevValue.current}</h1>
+    <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
+    <button onClick={focus}>Focus</button>
+  </>
+)
 }
 
 export default App;
