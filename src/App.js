629008be91736af27bd5a71459e56ebf5c0fd8de
Harovinkin <kharovinkin@gmail.com>
2020-11-19 15:14:21 +0200

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, {useState, useMemo} from 'react'
+
 import './App.css';
 
-function App() {
+const computeDelay = num => {
+  let i = 0
+  while (i < 1000000000) i += 1;
 
+  return num;
+}
+
+function App() {
+  const [number, setNumber] = useState(40);
+  const [color, setColor] = useState(true)
+
+  const computed = useMemo(() => {
+    return computeDelay(number)
+  }, [number]);
+
+  const styles = {
+    color: color ? 'black' : "blue"
+  }
+
+  return (
+    <>
+      <h1 style={styles}>Compute Result: {computed}</h1>
+      <button onClick={() => setNumber(prev => prev + 1)}>+1</button>
+      <button onClick={() => setNumber(prev => prev - 1)}>-1</button>
+      <button onClick={() => setColor(prev => !prev)}>Change Title Color</button>
+    </>
+  )
 }
 
 export default App;
