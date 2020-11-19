edb6273d0a1c3196ff7aedfbeba3959921f5a1b1
Harovinkin <kharovinkin@gmail.com>
2020-11-19 21:15:44 +0200

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, {useState, useCallback} from 'react'
 import './App.css';
+import ItemsList from './components/ItemsList';
 
 function App() {
+  const [count, setCount] = useState(1)
+  const [color, setColor] = useState(false)
 
+  const styles = {
+    color: color ? 'green' : 'black'
+  }
+
+  const generateItemsFromAPI = useCallback(() => {
+    return new Array(count).fill('').map((_, i) => `Element ${i + 1}`)
+  }, [count])
+
+  return (
+    <>
+      <h1 style={styles}>Elements Quantity: {count}</h1>
+      <button onClick={() => setCount(prev => prev + 1)}>Add 1</button>
+      <button onClick={() => setColor(prev => !prev)}>Change Color</button>
+
+      <ItemsList getItems={generateItemsFromAPI} />
+    </>
+  )
 }
 
 export default App;
