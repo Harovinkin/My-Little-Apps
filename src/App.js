97553bb9350d0bdeab6eef61c1e20cf0a6e7c861
Harovinkin <kharovinkin@gmail.com>
2020-11-24 01:11:33 +0200

    customHooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function useLogger(value) {
+  useEffect(() => {
+    console.log('Logger', value)
+  },[value])
+}
 
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = e => {
+    setValue(e.target.value )
+  }
+
+  return {
+    bind: {
+      value,
+      onChange
+    },
+    value
+  }
+}
+
+function App() {
+  const input = useInput('');
+
+  useLogger(input.value)
+
+  return(
+    <div>
+      <input type='text' {...input.bind} />
+      <h1>{input.value}</h1>
+    </div>
+  )
 }
 
 export default App;
